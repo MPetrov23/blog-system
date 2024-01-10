cc2f39e2795efa0952580d47151431c00b0ed282
MPetrov23 <maripetrov33@gmail.com>
2024-01-10 17:46:24 +0200

    Update SecurityConfig

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-
+                .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(
                         requests -> requests
                                 .requestMatchers("/", "/home",
